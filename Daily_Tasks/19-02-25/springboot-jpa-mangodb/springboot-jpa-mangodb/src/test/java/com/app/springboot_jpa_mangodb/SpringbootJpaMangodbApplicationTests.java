80848a7c65a11d6437741d5ef1ed27e2d44b4d71
SrinidhiBhoopati <118212453+SrinidhiBhoopati@users.noreply.github.com>
2025-02-25 14:08:30 +0530

    Add files via upload

---
+++ Daily_Tasks/19-02-25/springboot-jpa-mangodb/springboot-jpa-mangodb/src/test/java/com/app/springboot_jpa_mangodb/SpringbootJpaMangodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.springboot_jpa_mangodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaMangodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Daily_Tasks/19-02-25/springboot-jpa-mangodb/springboot-jpa-mysql/springboot-jpa-mysql/src/test/java/com/app/springboot_jpa_mysql/SpringbootJpaMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.springboot_jpa_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
